ed530d89a5b0464e2e0147c302e809d77ba8e334
joydeepdn <deepj0147@gmail.com>
2025-04-09 04:29:01 +0530

    some changes

--- animation.js
+++ animation.js
@@ -17,21 +17,21 @@ const observer = new IntersectionObserver((entries) => {
             entry.target.classList.add('animate__animated');
             if(entry.target.id == 'right-section'){
                 entry.target.classList.add('animate__bounceInDown')
-                observer.unobserve(entry.target);
+                observer.unobserve(entry.target)
             }
             else if (entry.target.id == 'about-me') {
-                entry.target.classList.add('animate__fadeIn','animate__delay-0.5s');
+                entry.target.classList.add('animate__bounceIn','animate__delay-1.5s');
             }
             else if (entry.target.id == 'technologies-list') {
-                entry.target.classList.add('animate__fadeIn','animate__delay-0.5s')
+                entry.target.classList.add('animate__fadeIn','animate__delay-1.7s')
             }
         }
         else {
-            entry.target.classList.remove('animate__animated', 'animate__bounceIn','animate__fadeIn');
+            entry.target.classList.remove('animate__animated', 'animate__bounceIn','animate__fadeIn','animate__bounceInDown');
         }
     })
 }, {
-    threshold: 0.5
+    threshold: 0.7
 })
 const elements = document.querySelectorAll('#about-me, #technologies-list, #right-section');
 elements.forEach(elements => observer.observe(elements));

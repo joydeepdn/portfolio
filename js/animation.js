ba811fcb956f1a2016a4cf32bd8820f17eeb0c92
joydeepdn <deepj0147@gmail.com>
2025-04-12 02:22:27 +0530

    added dark-mode

--- js/animation.js
+++ js/animation.js
@@ -1,45 +1,46 @@
 
-// window.onload = () => {
-
-//     const righSection = document.getElementById('right-section');
-//     const leftSection = document.getElementById('left-section');
-
-//     if (leftSection && screen.width > 600) {
-//         leftSection.classList.add('animate__animated', 'animate__bounceInDown');
-//     }
-//     else if (screen.width < 600) {
-//         righSection.classList.add('animate__animated', 'animate__bounceInDown')
-//     }
-// };
 const observer = new IntersectionObserver((entries) => {
     entries.forEach(entry => {
         if (entry.isIntersecting) {
             entry.target.classList.add('animate__animated');
-            if(entry.target.id == 'right-section'){
+            if (entry.target.id == 'right-section') {
                 entry.target.classList.add('animate__bounceInDown')
                 observer.unobserve(entry.target)
             }
             else if (entry.target.id == 'about-me-para') {
-                entry.target.classList.add('animate__flipInX','animate__delay-1.5s');
+                if (screen.width > 1023) {
+                    entry.target.classList.add('animate__flipInX', 'animate__delay-1.5s');
+                }
+                else if (screen.width < 1023) {
+                    entry.target.classList.add('animate__fadeIn', 'animate__delay-1.7s')
+                }
             }
             else if (entry.target.id == 'technologies-list') {
-                entry.target.classList.add('animate__fadeIn','animate__delay-1.7s')
+                entry.target.classList.add('animate__fadeIn', 'animate__delay-1.7s')
             }
-            else if(entry.target.id == 'contact-form' || entry.target.id == 'contact-links-section'){
+            else if (entry.target.id == 'contact-form' || entry.target.id == 'contact-links-section') {
                 entry.target.classList.add('animate__fadeIn');
             }
-            else if(entry.target.id == 'contact-para' || entry.classList.id == 'contact-links-1'){
+            else if (entry.target.id == 'contact-para') {
                 entry.target.classList.add('animate__flipInX');
             }
         }
         else {
-            entry.target.classList.remove('animate__animated', 'animate__flipInX','animate__fadeIn','animate__bounceInDown');
+            entry.target.classList.remove(
+                'animate__animated',
+                'animate__flipInX',
+                'animate__fadeIn',
+                'animate__fadeInDownBig',
+                'animate__bounceInDown'
+            );
         }
     })
 }, {
     threshold: 0.7
 })
-const elements = document.querySelectorAll('#about-me-para, #technologies-list, #right-section, #contact-form, #contact-links-section, #contact-para, #contact-links-1');
+const elements = document.querySelectorAll(
+    '#about-me-para,#technologies-list, #right-section, #contact-form, #contact-links-section, #contact-para'
+);
 elements.forEach(elements => observer.observe(elements));
 
 
--- js/script.js
+++ js/script.js
@@ -1,48 +1,84 @@
 const links = document.getElementById('links');
 const openBtn = document.getElementById('hamburger');
 const main = document.getElementById('main-div');
+const closeBtn = document.getElementById('close-button')
+const toggleBtn = document.getElementById('toggle-button');
+const sun = document.getElementById('sun')
+const moon = document.getElementById('moon');
+const body = document.body;
+const myNav = document.getElementById('mynav');
+
+
 
 function openNav() {
+  openBtn.style.display = 'none'
+  closeBtn.style.display = 'block'
 
   if (screen.width <= 600) {
-    links.style.width = "300px";
+    links.style.width = "250px";
   }
-  else if(screen.width > 600){
+  else if (screen.availWidth > 600) {
     links.style.width = '500px'
   }
-  if(openNav){
-    main.style.opacity = 0.5;
-  }
+  main.style.opacity = 0.5;
 }
-if(links.style.width > '0px'){
+if (links.style.width > '0px') {
   const eBody = document.body;
   eBody.style.opa
 }
 
 function closeNav() {
+  openBtn.style.display = 'block'
+  closeBtn.style.display = 'none'
   document.getElementById('links').style.width = "0";
   main.style.opacity = 1;
-} 
+}
 
-  openBtn.addEventListener('click', function(event){
-    openNav();
-    event.stopPropagation();
-  })
+openBtn.addEventListener('click', function (event) {
+  openNav();
+  event.stopPropagation();
+})
 
 document.addEventListener('click', function (event) {
-  if(links.style.width == 0){
+  if (links.style.width == 0) {
     return;
   }
-  else{
-  if (!links.contains(event.target) && links.style.width !== '0' && screen.width < 1023 ) {
-    closeNav();
+  else {
+    if (!myNav.contains(event.target) && !links.contains(event.target) && links.style.width !== '0' && screen.width < 1023) {
+      closeNav();
+    }
   }
-}
 });
 
 window.addEventListener('resize', () => {
-  const links = document.getElementById('links');
-  if (window.innerWidth > 600) {
+  if (window.innerWidth > 1023) {
     links.style.width = "";
+    openBtn.style.display = 'none';
+    closeBtn.style.display = 'none';
+  }
+  else if (screen.width <= 1024) {
+    openBtn.style.display = 'block';
+  }
+
+  if (closeBtn.style.display == 'block') {
+    openBtn.style.display = 'none';
+  }
+  if (window.innerWidth > 600 && links.style.width > '0px') {
+    links.style.width = '500px'
   }
 });
+
+toggleBtn.addEventListener('click', () =>{
+
+  body.classList.toggle('dark-mode')
+
+  if(body.classList.contains('dark-mode')){
+    sun.style.display = 'none';
+    moon.style.display = 'block'
+  }
+  else if(!body.classList.contains('dark-mode')){
+     sun.style.display = 'block';
+    moon.style.display = 'none'
+  }
+})
+
